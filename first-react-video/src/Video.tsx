ac2a9b8d7d3edc87802228a57488ed720b211336
Daynamarie <Dayna.christison@gmail.com>
2021-02-16 17:29:27 -0500

    dayna video black screen

--- first-react-video/src/Video.tsx
+++ first-react-video/src/Video.tsx
@@ -2,7 +2,7 @@ import {Composition} from 'remotion';
 import {HelloWorld} from './HelloWorld';
 import {Logo} from './HelloWorld/Logo';
 import {Subtitle} from './HelloWorld/Subtitle';
-
+import {DaynaVideo} from './DaynaVideo.jsx';
 export const RemotionVideo: React.FC = () => {
 	return (
 		<>
@@ -33,6 +33,18 @@ export const RemotionVideo: React.FC = () => {
 				fps={30}
 				width={1920}
 				height={1080}
+			/>
+			<Composition
+				id="DaynaVideo"
+				component={DaynaVideo}
+				durationInFrames={150}
+				fps={30}
+				width={1920}
+				height={1080}
+				defaultProps={{
+					titleText: 'This is Daynas Video',
+					titleColor: 'purple',
+				}}
 			/>
 		</>
 	);
